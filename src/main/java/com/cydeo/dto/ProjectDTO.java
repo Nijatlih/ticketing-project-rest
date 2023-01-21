080827d6b6b5ae8a31cb4e4e43cadf13a4969347
nijat <nijat.de@gmail.com>
2023-01-21 09:20:21 +0100

    ticketing project Part-0 dto

--- src/main/java/com/cydeo/dto/ProjectDTO.java
+++ src/main/java/com/cydeo/dto/ProjectDTO.java
@@ -4,10 +4,7 @@ import com.cydeo.enums.Status;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @NoArgsConstructor
@@ -17,24 +14,22 @@ public class ProjectDTO {
 
     private Long id;
 
-    @NotBlank
+
     private String projectName;
 
-    @NotBlank
+
     private String projectCode;
 
-    @NotNull
+
     private UserDTO assignedManager;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @NotNull
+
     private LocalDate startDate;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @NotNull
+
     private LocalDate endDate;
 
-    @NotBlank
+
     private String projectDetail;
 
     private Status projectStatus;
@@ -42,14 +37,5 @@ public class ProjectDTO {
     private int completeTaskCounts;
     private int unfinishedTaskCounts;
 
-    public ProjectDTO(String projectName, String projectCode, UserDTO assignedManager, LocalDate startDate, LocalDate endDate, String projectDetail, Status projectStatus) {
-        this.projectName = projectName;
-        this.projectCode = projectCode;
-        this.assignedManager = assignedManager;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.projectDetail = projectDetail;
-        this.projectStatus = projectStatus;
-    }
 
 }
